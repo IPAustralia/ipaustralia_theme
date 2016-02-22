3a1b05af27a86e77620420b667f9ec16986ee9b5
Brett <brett.thomson@ipaustralia.gov.au>
2016-02-23 10:42:03 +1100

    twitter feed

--- ipaustralia/js/script.js
+++ ipaustralia/js/script.js
@@ -3,3 +3,22 @@
         console.log('something');
     });
 })(jQuery);
+
+//add twitter feed script
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
+//end twitter feed script
