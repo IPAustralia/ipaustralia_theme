5fd1b1f5ca1b526939b6604fba2cfb64bcc392e7
Brett <brett.thomson@ipaustralia.gov.au>
2016-03-29 13:46:25 +1100

    New js for Josh

--- ipaustralia/interactives/infographic/js/infographic.js
+++ ipaustralia/interactives/infographic/js/infographic.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
+jQuery(document).ready(function(){
+	jQuery('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
 	    var target = this.hash;
-	    var $target = $(target);
+	    var jQuerytarget = jQuery(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	    jQuery('html, body').stop().animate({
+	        'scrollTop': jQuerytarget.offset().top
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
 
-var $animation_elements = $('.animation-element');
-var $window = $(window);
+var jQueryanimation_elements = jQuery('.animation-element');
+var jQuerywindow = jQuery(window);
 
 function check_if_in_view() {
-var window_height = $window.height();
-var window_top_position = $window.scrollTop();
+var window_height = jQuerywindow.height();
+var window_top_position = jQuerywindow.scrollTop();
 var window_bottom_position = (window_top_position + window_height);
  
-$.each($animation_elements, function() {
-var $element = $(this);
-var element_height = $element.outerHeight();
-var element_top_position = $element.offset().top;
+jQuery.each(jQueryanimation_elements, function() {
+var jQueryelement = jQuery(this);
+var element_height = jQueryelement.outerHeight();
+var element_top_position = jQueryelement.offset().top;
 var element_bottom_position = (element_top_position + element_height);
  
 //check to see if this current container is within viewport
 if ((element_bottom_position >= window_top_position) &&
 (element_top_position <= window_bottom_position)) {
-$element.addClass('in-view');
+jQueryelement.addClass('in-view');
 } else {
-$element.removeClass('in-view');
+jQueryelement.removeClass('in-view');
 }
 });
 }
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+jQuerywindow.on('scroll resize', check_if_in_view);
+jQuerywindow.trigger('scroll');
 	
 });
