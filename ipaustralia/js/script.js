b3adcd94f5222e5bf53805fa1cda8fc0c4a17fd0
Brett <brett.thomson@ipaustralia.gov.au>
2016-04-13 12:51:22 +1000

    Sticky header for ip infringement landing nav

--- ipaustralia/js/script.js
+++ ipaustralia/js/script.js
@@ -623,7 +623,7 @@ function stickyMenu() {
         jQuery("#block-bean-trade-marks-anchor-menu").trigger("sticky_kit:detach");
         jQuery("#block-bean-designs-anchor-menu").trigger("sticky_kit:detach");
         jQuery("#block-bean-pbr-anchor-menu").trigger("sticky_kit:detach");
-        jQuery("#block-bean-ip-infringement-anchor-block").trigger("sticky_kit:detach");
+        jQuery("#block-bean-ip-infringement-anchor-block-0").trigger("sticky_kit:detach");
         jQuery("#block-bean-understanding-ip-anchor-block-0").trigger("sticky_kit:detach");
         jQuery("#block-bean-about-us-anchor-menu").trigger("sticky_kit:detach");
     } else {
@@ -632,7 +632,7 @@ function stickyMenu() {
         jQuery("#block-bean-trade-marks-anchor-menu").stick_in_parent();
         jQuery("#block-bean-designs-anchor-menu").stick_in_parent();
         jQuery("#block-bean-pbr-anchor-menu").stick_in_parent();
-        jQuery("#block-bean-ip-infringement-anchor-block").stick_in_parent();
+        jQuery("#block-bean-ip-infringement-anchor-block-0").stick_in_parent();
         jQuery("#block-bean-understanding-ip-anchor-block-0").stick_in_parent();
         jQuery("#block-bean-about-us-anchor-menu").stick_in_parent();
     }
