65b9c310a1d1560030e3f0378483416027254b16
Brett <brett.thomson@ipaustralia.gov.au>
2016-06-28 11:09:25 +1000

    Other readspeaker not working, trying out this one.

--- ipaustralia/templates/page.tpl.php
+++ ipaustralia/templates/page.tpl.php
@@ -1,10 +1,4 @@
 
-<?php
- /* $theme_path = drupal_get_path('theme', 'ipaustralia');
-  function hook_preprocess_page(&$vars) {
-  drupal_add_js($theme_path . '/readspeaker/ReadSpeaker.js?pids=embhl');
-}*/ ?>
-
 <!-- ClickTale Top part -->
 <script type="text/javascript">
 var WRInitTime=(new Date()).getTime();
@@ -58,7 +52,7 @@ var WRInitTime=(new Date()).getTime();
       <a id="main-content"></a>
       <!-- begin readspeaker implementation -->
       <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve">
-        <a rel="nofollow" class="rsbtn_play" accesskey="L" title="Listen to this page using ReadSpeaker" href="https://app-as.readspeaker.com/cgi-bin/rsent?customerid=6064&amp;lang=en_au&amp;readid=block-system-main&amp;url=<?php print $_SERVER['HTTP_HOST'] . request_uri();?>">
+        <a rel="nofollow" class="rsbtn_play" accesskey="L" title="Listen to this page using ReadSpeaker" href="https://app-as.readspeaker.com/cgi-bin/rsent?customerid=6064&amp;lang=en_au&amp;readid=block-system-main&amp;url=<?php print urlencode("https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);?>">
               <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Listen to this page</span></span></span>
               <span class="rsbtn_right rsimg rsplay rspart"></span>
           </a>
