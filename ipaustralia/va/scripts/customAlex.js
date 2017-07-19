510d98f96e8449274076474249e9bf01698056eb
Brett Thomson <brett_t@iinet.net.au>
2017-07-19 15:04:48 +1000

    Update customAlex.js
    Enabling the preprod variable as per the release package.

--- ipaustralia/va/scripts/customAlex.js
+++ ipaustralia/va/scripts/customAlex.js
@@ -10,7 +10,7 @@ NinaVars = {
 if(getParameterByName('preprod')) {
    NinaVars.preprod = true;
 }
-/*   NinaVars.preprod = true;*/
+NinaVars.preprod = true; //Disable for production release
 
 function getParameterByName(name, url) {
     //this function is typically used to return a boolean
