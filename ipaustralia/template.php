f376233b1a42c604d70e8d1f4aeac59058b53ad0
IP Australia <webadmin@ipaustralia.gov.au>
2017-08-18 09:31:17 +1000

    remove the testing code

--- ipaustralia/template.php
+++ ipaustralia/template.php
@@ -113,7 +113,7 @@ function ipaustralia_preprocess_page(&$vars, $hook) {
  * Implements hook_theme_form_alter()
  */
 function ipaustralia_form_alter(&$form, &$form_state, $form_id) {
-  echo '<pre>' . var_dump($form) . '</pre>';
+  //echo '<pre>' . var_export($form) . '</pre>';
 }
 
 /**
