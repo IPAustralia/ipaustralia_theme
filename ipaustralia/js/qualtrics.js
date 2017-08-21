5515f5ad6a83d028c58eb28a241d3b96f564aacc
IP Australia <webadmin@ipaustralia.gov.au>
2017-08-21 15:38:45 +1000

    release qualtrics.js to prod

--- ipaustralia/js/qualtrics.js
+++ ipaustralia/js/qualtrics.js
@@ -1,7 +1,3 @@
-(function() {
-    $('body').append("<div id='ZN_01epb97w0hDoY4t'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>");
-})();
-
 (function(){var g=function(e,h,f,g){
 this.get=function(a){for(var a=a+"=",c=document.cookie.split(";"),b=0,e=c.length;b<e;b++){for(var d=c[b];" "==d.charAt(0);)d=d.substring(1,d.length);if(0==d.indexOf(a))return d.substring(a.length,d.length)}return null};
 this.set=function(a,c){var b="",b=new Date;b.setTime(b.getTime()+6048E5);b="; expires="+b.toGMTString();document.cookie=a+"="+c+b+"; path=/; "};
@@ -9,3 +5,7 @@ this.check=function(){var a=this.get(f);if(a)a=a.split(":");else if(100!=e)"v"==
 this.go=function(){if(this.check()){var a=document.createElement("script");a.type="text/javascript";a.src=g+ "&t=" + (new Date()).getTime();document.body&&document.body.appendChild(a)}};
 this.start=function(){var a=this;window.addEventListener?window.addEventListener("load",function(){a.go()},!1):window.attachEvent&&window.attachEvent("onload",function(){a.go()})}};
 try{(new g(100,"r","QSI_S_ZN_01epb97w0hDoY4t","//zn01epb97w0hdoy4t-ipaustralia.siteintercept.qualtrics.com/SIE/?Q_ZID=ZN_01epb97w0hDoY4t&Q_LOC="+encodeURIComponent(window.location.href))).start()}catch(i){}})();
+
+
+var divQualtrix = "<div id='ZN_01epb97w0hDoY4t'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>";
+document.body.appendChild(divQualtrix);
