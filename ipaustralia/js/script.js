41154eb8c8f992b80d2013f3ca98cbbdebe84a46
IP Australia <webadmin@ipaustralia.gov.au>
2017-08-23 12:06:53 +1000

    add match height and popupoverlay

--- ipaustralia/js/script.js
+++ ipaustralia/js/script.js
@@ -18,7 +18,16 @@ jQuery(document).ready(function () {
     jQuery('body').append("<div id='ZN_0e0bzgMZ1MJCD1H'><!--DO NOT REMOVE-CONTENTS PLACED HERE--></div>");
 
     //jQuery("#dialog").dialog();
+    jQuery('#dialog').notifyModal({
+        duration : 2500,
+        placement : 'center',
+        onTop : true,
+    });
 });
+
+
+
+
 jQuery(document).on('click', '.bp-small #block-bean-tools-and-resources-generic-bloc h3', function () {
     openCloseToolsResources(jQuery(this));
 });
