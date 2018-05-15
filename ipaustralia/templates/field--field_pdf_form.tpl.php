26d5a37039160033d22556d423bea7353a4cda10
Mike Kreuzer <mikekreuzer@users.noreply.github.com>
2018-05-15 15:53:59 +1000

    PDF form test
    /templates/field--field_online_services_link.tpl.php
    /templates/field--field_pdf_form.tpl.php

--- ipaustralia/templates/field--field_pdf_form.tpl.php
+++ ipaustralia/templates/field--field_pdf_form.tpl.php
@@ -2,8 +2,10 @@
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-        <p>Or fill in and submit this form via post: <?php print render($item); ?></p>
-        <p>Please check the time and costs pages for the latest fees.</p>
+        <h2>Download form</h2>
+        <ul>
+          <li><?php print render($item); ?></li>
+        </ul>
       </div>
     <?php endforeach; ?>
   </div>
