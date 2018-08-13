d03f54d00ecc54afbe0e3516c8782794e10fd8c8
Mike Kreuzer <mikekreuzer@users.noreply.github.com>
2018-08-13 11:37:16 +1000

    Rolling back digital IP commit
    templates\node--digital-ip.tpl.php
    interactives\digital-ip\digital-ip.js
    
    (Both removed)

--- ipaustralia/templates/node--digital-ip.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // standard page
-  include DRUPAL_ROOT . "/" . path_to_theme() . "/templates/page.tpl.php";
-
-  // content type specific loads
-  $digital_ip_path = drupal_get_path('theme', 'ipaustralia') . '/interactives/digital-ip';
-  drupal_add_js($digital_ip_path . '/digital-ip.js', array('type' => 'file', 'scope' => 'footer'));
-?>
